8fe9135da7adcfae657c19189cd3bcf794cc3bf2
Ali Kaif <aliakaif@email.arizona.edu>
2019-12-03 19:42:42 -0700

    Create trees.java

---
+++ trees.java
@@ -0,0 +1,74 @@
+public class Tree {
+  
+  public static void main(String[] args) {
+    //Let's plant a tree!
+    System.out.println("Hi there! My name is Woody! Let's plant a tree and help out the planet!");
+    
+    System.out.println();
+    
+    System.out.println("Here are some trees we can code and plant!");
+      
+    System.out.println();
+    System.out.println();
+    
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+    
+    System.out.println();
+    System.out.println();
+
+    
+    System.out.println("* ** *");
+    System.out.println("*****");
+    System.out.println(" **** ");
+    System.out.println("  **  ");
+    System.out.println("  **  ");  
+    System.out.println("~~~~~~");
+    
+    System.out.println();
+    System.out.println();
+
+    System.out.println("*****");
+    System.out.println("*****");
+    System.out.println("*****");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+    System.out.println("#####");
+    
+    System.out.println();
+    System.out.println();
+    
+    
+    System.out.println("******");
+    System.out.println("*****");
+    System.out.println(" **** ");
+    System.out.println("  **  ");
+    System.out.println("  **  ");  
+    System.out.println("~~~~~~");
+    
+    System.out.println();
+    System.out.println();
+    
+    
+    System.out.println("   *  ");
+    System.out.println("  / \");
+    System.out.println(" /   \");
+    System.out.println("/     \");
+    System.out.println("-------");
+    System.out.println("  | |    ");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      
+  }
+}
